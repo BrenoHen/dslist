02d1879ddedf83bde1e9fe99b0e897c4677a853b
Breno <breno.devs@gmail.com>
2025-01-22 10:48:26 -0300

    GET game by id

--- src/main/java/ia/breno/dslist/controller/GameController.java
+++ src/main/java/ia/breno/dslist/controller/GameController.java
@@ -1,10 +1,12 @@
 package ia.breno.dslist.controller;
 
+import ia.breno.dslist.dto.GameDTO;
 import ia.breno.dslist.dto.GameMinDTO;
 import ia.breno.dslist.entities.Game;
 import ia.breno.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
